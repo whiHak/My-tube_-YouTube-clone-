e6d99dcd54b57c9938c677ffffe8d0aefe7605ca
whiHak <betse1111111111@gmail.com>
2023-10-19 19:26:58 +0300

    modified styles on video card

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -14,29 +14,29 @@ const VideoCard = ({ video, id }) => {
   return (
     <Card
       sx={{
-        width: { md: "300px", xs: "100%" },
+        width: { sx: "100%", md: "300px" },
         border: "none",
         borderRadius: "0",
-        boxShadow: "none ",
+        boxShadow: "none "
       }}
     >
       <CardActionArea>
-        <Link to={id && `/video/${id}`}>
+        <Link to={id && `/video/${id.videoId}`}>
           <CardMedia
             component="img"
             image={video?.thumbnails?.medium?.url}
             alt={video.title}
-            sx={{ height: "auto", width: { md: "300px", xs: "100%" } }}
+            sx={{ height: "auto", width: { sx: "100%", md: "300px" } }}
           />
         </Link>
         <CardContent sx={{ backgroundColor: "#1e1e1e", height: "100px" }}>
-          <Link to={id && `/video/${id}`}>
+          <Link to={id && `/video/${id.videoId}`}>
             <Typography
               varient="subtitle1"
               color="white"
-              sx={{ p: "10", fontSize: "16px", mb: "10px" }}
+              sx={{ p: "10", fontSize: "16px", mb: "10px",fontWeight:"bold" }}
             >
-              {video.title}
+              {video.title.slice(0, 70)}
             </Typography>
           </Link>
           <Link to={id && `/channel/${video.channelId}`}>
@@ -45,7 +45,7 @@ const VideoCard = ({ video, id }) => {
               sx={{
                 p: "10",
                 color: "#AFAFAF",
-                fontSize: "15px",
+                fontSize: "12px",
                 alignItems: "center",
               }}
             >
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,23 +2,26 @@ import { Box, Stack } from "@mui/material";
 import React, { useEffect } from "react";
 import { VideoCard, ChannelCard } from "./component";
 
-const Videos = ({videos}) => {
+const Videos = ({ videos }) => {
   return (
     <Stack
       direction="row"
       alignItems="center"
-      display="flex"
       flexDirection="start"
       flexWrap="wrap"
-      justifyContent="start"
-      gap={3}
+      gap={5}
+      sx={{  display:"flex", justifyContent:"center", alignItems:"center",margin:{sx:0, md:5 } }}
     >
-    {console.log(videos)}
-      {videos.map((items, indx) =>(
+      {console.log(videos)}
+      {videos?.map((items, indx) => (
         <Box key={indx}>
-        {/* {console.log(items.snippet.thumbnails.medium.url)} */}
-          {items?.id?.channelId && <ChannelCard channel = {items.snippet} id={items.id}/>}
-          {items?.id?.videoId && <VideoCard video = {items.snippet} id={items.id}/>}
+          {/* {console.log(items.snippet.thumbnails.medium.url)} */}
+          {items?.id?.channelId && (
+            <ChannelCard channel={items.snippet} id={items.id} alignSelf="center" />
+          )}
+          {items?.id?.videoId && (
+            <VideoCard video={items.snippet} id={items.id} />
+          )}
         </Box>
       ))}
     </Stack>
