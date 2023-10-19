510d3c204d60eba3a71ec4b8aba9726ae976d046
whiHak <betse1111111111@gmail.com>
2023-10-19 20:27:07 +0300

    modified searchFeed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,14 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { Paper, IconButton } from "@mui/material";
 import { Search } from "@mui/icons-material";
+import { Link, useNavigate } from "react-router-dom";
+
 const SearchBar = () => {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const handleKeyDown = (e) => {
+    e.code === "Enter" && navigate(`/search/${query}`);
+  };
   return (
     <Paper
       component="form"
@@ -13,21 +19,31 @@ const SearchBar = () => {
         border: "1px solid #e3e3e3",
         boxShadow: "none",
         mr: { sm: 5 },
-        height:"35px",
-        display:"flex",
-        alignItems:"center",
-        width: "30%"
+        height: "35px",
+        display: "flex",
+        alignItems: "center",
+        width: "30%",
       }}
     >
       <input
         type="text"
         placeholder="Search..."
-        style={{ border: "none", outline: "none", borderRadius: "20px", width:"100%" }}   
-        onChange={()=>{}}
+        style={{
+          border: "none",
+          outline: "none",
+          borderRadius: "20px",
+          width: "100%",
+        }}
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+        onKeyDown={handleKeyDown}
       />
-      <IconButton>
-        <Search type="submit" sx={{ p:"10px", color:"#00A9EC"}}/>
-      </IconButton>
+      <Link to={`/search/${query}`}>
+        <IconButton>
+          <Search type="submit" sx={{ p: "10px", color: "#00A9EC" }} />
+        </IconButton>
+      </Link>
     </Paper>
   );
 };
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Box, Stack, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchAPI } from "../utils/fetchAPI";
+import Videos from "./Videos";
 
 const SearchFeed = () => {
-  return (
-    <div>SearchFeed</div>
-  )
-}
+  const { searchTerm } = useParams();
+  console.log(searchTerm);
+  const [videos, setVideos] = useState([]);
 
-export default SearchFeed
+  useEffect(() => {
+    fetchAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => {
+        setVideos(data.items);
+      })
+      .catch((err) => {
+        //Handle error
+      });
+  }, []);
+  return (
+    <Stack sx={{ flexDirection:"column", alignItems:"center"}}>
+      <Typography variant="h4" sx={{ mb: 2, color: "white", height:"max-content"}}>
+        Search results for{" "}
+        <span style={{ color: "#00A9EC", fontWeight: "bold" ,height:"max-content"}}>
+          "{searchTerm}"
+        </span>
+      </Typography>
+      <Box sx={{ p: 2, overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
+export default SearchFeed;
